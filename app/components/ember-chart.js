9d92dfdfa0b34d6dba04788d8bbf9bf72c4654b8
Ahmed O <aomra015@gmail.com>
2014-09-13 21:28:53 -0400

    fix file indentation

--- app/components/ember-chart.js
+++ app/components/ember-chart.js
@@ -2,18 +2,18 @@ import Ember from 'ember';
 /* global Chart */
 
 export default Ember.Component.extend({
-	tagName: 'canvas',
-	attributeBindings: ['width', 'height'],
+  tagName: 'canvas',
+  attributeBindings: ['width', 'height'],
 
-	didInsertElement: function(){
+  didInsertElement: function(){
     this.renderChart();
-	},
-	renderChart: function(){
-		var context = this.get('element').getContext('2d');
+  },
+  renderChart: function(){
+    var context = this.get('element').getContext('2d');
     var data = this.get('data');
     var type = this.get('type').capitalize();
     var options = Ember.merge({}, this.get('options'));
 
     new Chart(context)[type](data, options);
-	}.observes('data', 'options')
+  }.observes('data', 'options')
 });
