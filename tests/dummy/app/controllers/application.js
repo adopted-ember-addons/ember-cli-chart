539a8d8300187a9bc905c1a3c015baeb19f78e0c
Ahmed O <aomra015@gmail.com>
2014-11-10 14:50:57 -0500

    version bump + dummy tests

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,52 +1,61 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	pieData: [
-    {
-      value: 300,
-      color:"#F7464A",
-      highlight: "#FF5A5E",
-      label: "Red"
-    },
-    {
-      value: 50,
-      color: "#46BFBD",
-      highlight: "#5AD3D1",
-      label: "Green"
-    },
-    {
-      value: 100,
-      color: "#FDB45C",
-      highlight: "#FFC870",
-      label: "Yellow"
-    }
-	],
-	lineData: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
-},
-	barData: {
+  pieValue1: 300,
+  pieValue2: 50,
+  pieValue3: 100,
+  pieData: function(){
+    return [
+      {
+        value: parseInt(this.get('pieValue1')),
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+      },
+      {
+        value: parseInt(this.get('pieValue2')),
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+      },
+      {
+        value: parseInt(this.get('pieValue3')),
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+      }
+    ];
+  }.property('pieValue1', 'pieValue2', 'pieValue3'),
+    lineValue1: 65,
+  lineValue2: 59,
+  lineData: function(){
+    return {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [parseInt(this.get('lineValue1')), parseInt(this.get('lineValue2')), 80, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
+  }.property('lineValue1', 'lineValue2'),
+    barData: {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
         {
@@ -67,7 +76,7 @@ export default Ember.Controller.extend({
         }
     ]
 },
-	radarData: {
+    radarData: {
     labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
     datasets: [
         {
@@ -92,7 +101,7 @@ export default Ember.Controller.extend({
         }
     ]
 },
-	polarAreaData: [
+    polarAreaData: [
     {
         value: 300,
         color:"#F7464A",
