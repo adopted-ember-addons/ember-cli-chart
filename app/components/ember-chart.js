afc483fdbb6ecac3ef0173e2b8deb753db93c1b0
Ahmed O <aomra015@gmail.com>
2015-02-22 13:41:05 -0500

    extract two methods - one for updating linear graphs and one for pie charts

--- app/components/ember-chart.js
+++ app/components/ember-chart.js
@@ -36,39 +36,12 @@ export default Ember.Component.extend({
       var needUpdate = false;
       // Line, Bar and Radar charts
       if (data.datasets) {
-        data.datasets.forEach(function(dataset, i) {
-          dataset.data.forEach(function(item, j) {
-            if (typeof chart.datasets[i] === 'undefined') {
-              chart.segments[j].value = item;
-            } else {
-              var dataSet = chart.datasets[i];
-
-              if(typeof dataSet.bars !== 'undefined') {
-                chart.datasets[i].bars[j].value = item;
-              } else {
-                chart.datasets[i].points[j].value = item;
-              }
-            }
-          });
-        });
-        needUpdate = true;
+        needUpdate = this.updateLinearCharts(data, chart)
       }
       else {
         // Polar, Pie and Doughnut charts use an array as their data source
         if (Array.isArray(data)) {
-          data.forEach(function(segment, i) {
-            if (typeof chart.segments[i] !== 'undefined') {
-              if (chart.segments[i].value != segment.value) {
-                chart.segments[i].value = segment.value;
-                needUpdate = true;
-              }
-            }
-            else {
-              // there are now more segments than the chart knows about; add them
-              chart.addData(segment, i, true);
-              needUpdate = true;
-            }
-          });
+          needUpdate = this.updatePieCharts(data, chart)
         }
       }
 
@@ -78,8 +51,48 @@ export default Ember.Component.extend({
       }
     } catch(error) {
       Ember.warn('Dataset is not equal in structure as previous values. Rebuilding chart...');
+      console.error(error);
       this.destroyChart();
       this.renderChart();
     }
-  }.observes('data', 'data.[]', 'options')
+  }.observes('data', 'data.[]', 'options'),
+
+  updateLinearCharts: function (data, chart) {
+    data.datasets.forEach(function(dataset, i) {
+      dataset.data.forEach(function(item, j) {
+        item = item || 0;
+        if (typeof chart.datasets[i] === 'undefined') {
+          chart.segments[j].value = item;
+        } else {
+          var dataSet = chart.datasets[i];
+
+          if(typeof dataSet.bars !== 'undefined') {
+            chart.datasets[i].bars[j].value = item;
+          } else {
+            chart.datasets[i].points[j].value = item;
+          }
+        }
+      });
+    });
+    return true;
+  },
+
+  updatePieCharts: function (data, chart) {
+    var needUpdate = false;
+    data.forEach(function(segment, i) {
+      if (typeof chart.segments[i] !== 'undefined') {
+        segment.value = segment.value || 0
+        if (chart.segments[i].value != segment.value) {
+          chart.segments[i].value = segment.value;
+          needUpdate = true;
+        }
+      }
+      else {
+        // there are now more segments than the chart knows about; add them
+        chart.addData(segment, i, true);
+        needUpdate = true;
+      }
+    });
+    return needUpdate
+  }
 });
