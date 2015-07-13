065c712e9d1e32337f37f6a0b20c4ba52d9b73ec
Scott Batson <sbatson@MM35758PC.local>
2015-07-13 10:46:50 -0400

    Bar chart redraws upon dataset change

--- addon/chart-data-updater.js
+++ addon/chart-data-updater.js
@@ -18,6 +18,10 @@ export default Ember.Object.extend({
 
     if (chart.datasets.length !== datasets.length) {
       return this.set('redraw', true);
+    } else if (typeof chart.datasets[0].bars !== 'undefined') {
+      if (chart.datasets[0].bars.length !== datasets[0].data.length) {
+        return this.set('redraw', true);
+      }
     }
 
     // Update Labels
