f4e56a738c0734a1828a2b9752ea5fe22aaf8227
Devonte <devo.tox.89@gmail.com>
2017-06-03 20:37:42 +0100

    Use the find host method

--- index.js
+++ index.js
@@ -3,15 +3,24 @@
 
 module.exports = {
   name: 'ember-cli-chart',
-  included: function(app, parentAddon) {
+  included: function() {
     this._super.included.apply(this, arguments);
-    
-    var target = (parentAddon || app);
-    if (target.app) {
-      target = target.app;
+
+    var app;
+
+    // If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
+    // use that.
+    if (typeof this._findHost === 'function') {
+      app = this._findHost();
+    } else {
+      // Otherwise, we'll use this implementation borrowed from the _findHost()
+      // method in ember-cli.
+      var current = this;
+      do {
+        app = current.app || app;
+      } while (current.parent.parent && (current = current.parent));
     }
     
-    var bowerDir = target.bowerDirectory;
-    app.import(bowerDir + '/chartjs/dist/Chart.js');
+    app.import(app.bowerDirectory + '/chartjs/dist/Chart.js');
   }
 };
